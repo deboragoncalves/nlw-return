ac7905210aec375cab9f5ba9b785fc1646900022
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-05-04 22:41:04 -0300

    Finalizar popover usando lib headless ui

--- nlw-return-web/src/components/Widget.tsx
+++ nlw-return-web/src/components/Widget.tsx
@@ -1,13 +1,25 @@
 import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from "@headlessui/react";
 
 export function Widget() {
-    /*  Tailwind: 
+    /*  
+        Tailwind: 
             - w-6 = 1 representa 4px
             - px: padding lateral, eixo x
+
+        React:
+            - className
+            - nome componente sempre inicia com letra maiuscula
+            - escrever JS dentro do HTML: {}
+            - acessibilidade: atributo aria-expanded tem valor true/false e indica para o leitor de tela que o item está aparecendo ou não
+            - libs criam esse atributo automaticamente e facilitam a navegabilidade
     */ 
     return (
-        <button className="bg-violet-500 rounded full px-3 h-12 text-white">
-            <ChatTeardropDots className="w-6 h-6" />
-        </button>
+        <Popover className="absolute bottom-4 right-4 group">
+            <Popover.Button className="bg-violet-500 rounded full px-3 h-12 text-white flex items-center">
+                <ChatTeardropDots className="w-6 h-6" />
+                <Popover.Panel className="px-2 hidden group-hover:block transition-all duration-500 ease-linear">Feedback</Popover.Panel>
+            </Popover.Button>
+        </Popover>
     )
 }
--- nlw-return-web/tailwind.config.js
+++ nlw-return-web/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      // Criar cor e classe personalizada no tailwind, criando um novo objeto (bg-brand-500)
+      colors: {
+        brand: {
+          500: '#8257e6'
+        }
+      }
+    },
   },
   plugins: [],
 }
