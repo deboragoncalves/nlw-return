c262c589867fbf4102540ef6147e732f92ab05ad
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-05-08 15:28:01 -0300

    Corrigir bug no componente FeedbackTypeStep

--- nlw-return-web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ nlw-return-web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -4,10 +4,12 @@ interface FeedbackTypeStepProps {
 }
 
 // Quando é uma função passada como props, devo declarar o tipo de parâmetro e de retorno
+// Componente recebe como props o objeto com os itens do feedback e a função de set do useState
 
 export function FeedbackTypeStep(prop: FeedbackTypeStepProps) {
     let feedbackTypes = prop.feedbackTypes;
-    let setFeedbackType = prop.onFeedbackTypeChanged(null);
+
+    type FeedbackType = keyof typeof feedbackTypes;
 
     return (
         <div className="flex py-8 gap-2 w-full">
@@ -18,7 +20,7 @@ export function FeedbackTypeStep(prop: FeedbackTypeStepProps) {
                 return (
                     <button
                         key={feedback}
-                        onClick={() => setFeedbackType(feedback)}
+                        onClick={() => prop.onFeedbackTypeChanged(feedback as FeedbackType)}
                         className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-550 focus:outline-none"
                     >
                         <img src={feedbackItems.image.source} alt={feedbackItems.image.alt}></img>
